a844b1c18037e6bd1adfc899dbc7ce39288113ac
ayyalusuriya <ayyalusuriya04@gmail.com>
2026-06-29 09:29:27 +0530

    More practice

---
+++ src/Palindrome.java
@@ -0,0 +1,20 @@
+public class Palindrome {
+
+    static void palindrome(int num){
+        int res = 0;
+        int temp = num;
+
+        while (num!=0){
+            int last= num%10;
+            res = (res*10) + last;
+            num/=10;
+        }
+        System.out.println(temp == res? "palindrome" : "Not a palindrome");
+    }
+
+    public static void main(String[] args) {
+        int num = 1234;
+        palindrome(121);
+
+    }
+}
